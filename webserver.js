a19e89612abadfdc2ba4cb7cfaa4769c7998bddb
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-10-25 01:27:55 -0200

    aaaaaaaaaaaaaa

--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const staticPath = path.join(__dirname, '/public');
+const staticPath = path.join(__dirname, '/fotos');
 const port = process.env.PORT || 19998;
 
 const server = express().use(express.static(staticPath));
